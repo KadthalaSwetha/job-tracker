1964b4c4fe016ccaf0334ba30260f06f76d0ef32
Kadthala Swetha <swethareddy6727@gmail.com>
2026-09-22 13:37:24 +0530

    add path variable and request parameter handling

--- src/main/java/com/job_tracker/controller/JobController.java
+++ src/main/java/com/job_tracker/controller/JobController.java
@@ -1,7 +1,9 @@
 package com.job_tracker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.job_tracker.Job;
@@ -16,6 +18,16 @@ public class JobController {
      @GetMapping("/jobs")
     public String getJobs() {
         return "Here are your jobs";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public String getJobById(@PathVariable int id){
+        return "you requested for the jobs with id :"+id;
+    }
+
+    @GetMapping("/jobs/search")
+    public String status(@RequestParam String status){
+        return "Searching jobs with status:" +status;
     }
 
     @PostMapping("/jobs")
